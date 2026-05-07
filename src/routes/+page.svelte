87553f78fe21fc378bc3a67ea3c5c737996de20b
Lawjay Lee <lawjaylee1@icloud.com>
2026-05-07 02:39:53 -0700

    ui: fix github logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,17 +8,20 @@
   let fetchData: Promise<string> | undefined = $state()
 </script>
 
-<main class="grid min-h-dvh grid-cols-1 place-content-center p-8">
+<header>
   <a
     href="https://github.com/Lomzem/viewsource"
     target="_blank"
     rel="noreferrer"
     aria-label="View on GitHub"
-    class="absolute top-8 left-12 flex -translate-x-1/2 text-muted-foreground transition hover:text-foreground"
+    class="mt-4 ml-4 flex gap-1 text-muted-foreground transition hover:text-foreground"
   >
-    <IconBrandGithubFilled size={32} />
+    <IconBrandGithubFilled />
+    <span class="self-baseline-last">view on github</span>
   </a>
+</header>
 
+<main class="grid min-h-dvh grid-cols-1 place-content-center p-8">
   <h1 class="text-center font-heading text-5xl font-black text-pretty">put the html in the bag</h1>
   <form
     class="mt-8 flex w-full flex-col gap-3 place-self-center sm:w-3xl sm:flex-row sm:items-center"
